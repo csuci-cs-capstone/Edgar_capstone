ce00a025a4df294dd03209782ac3d19f0504a6e0
motoredge <56284291+motoredge@users.noreply.github.com>
2022-04-27 19:59:28 -0700

    Add files via upload

--- server2.js
+++ server2.js
@@ -1,20 +1,35 @@
 const express = require('express')
 const app = express()
+var mysql = require('mysql');
 const path = require('path');
 const port = 3000
 const file = "index.html";
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, file), function (err){
-        if (err) {
-            next(err);
-        }
-        else {
-            console.log('Sent:', file);
-        }
-    })
-})
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "@27r0b0+=",
+    database: "football"
+});
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to MySQL!");
+
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, file), function (err){
+            if (err) {
+                 next(err);
+            }
+            else {
+                console.log('Sent:', file);
+            }
+        })
+    })
+
+    con.end();
+});
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`)
+    })
