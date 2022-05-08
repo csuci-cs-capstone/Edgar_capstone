4527c22f8416e3d1ed0c95ee1a6d55d7e5213e4f
motoredge <motoredge@LAPTOP-RMN3LKU5.localdomain>
2022-05-07 19:00:45 -0700

    adding healthcheck

--- server2.js
+++ server2.js
@@ -8,11 +8,19 @@ var bodyParser = require('body-parser')
 var mysql = require('mysql');
 var urlencodedParser = bodyParser.urlencoded({ extended: true })
 
+//var con = mysql.createConnection({
+//    host: "localhost",
+//    user: "root",
+//    password: "@27r0b0+=",
+//    database: "football",
+//    multipleStatements: true
+//});
+
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "@27r0b0+=",
-    database: "football",
+    host: "fantasy-football.cejgj6f569iv.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "password",
+    database: "fantasy_football",
     multipleStatements: true
 });
 
@@ -32,30 +40,34 @@ con.connect(function (err) {
         })
     })
 
+    app.get('/healthcheck', (req, res) => {
+            if (err) {
+                next(err);
+            }
+            else {
+                res.send("healthy")
+            }
+    })
+
     app.post('/analyze', urlencodedParser, (req, res) => {
-        var sql = `call grab_stats("${req.body.player1}")`;
-        var sql2 = `call grab_stats("${req.body.player2}")`;
+        var sql = `call get_stats("${req.body.player1}");`;
+        var sql2 = `call get_stats("${req.body.player2}")`;
+        var multiplesql = sql + sql2;
         console.log(sql);
         console.log(sql2);
-
-        con.query(sql, function (err, result) {
+        console.log(multiplesql);
+        con.query(multiplesql, function (err, result) {
             if (err) throw err;
-            console.log("Result(s) from query: ")
-            console.log(result[0]);
-            res.write(result[0].toString());
-            //con.end();
+            console.log("Result(s) from query: ");
+            console.log(result);
+            //const obj = JSON.parse(result[0]);
+            //console.log(obj.name);
+            //console.log(result);
+            //res.write(result[0].toString());
+            res.send(result);
+            con.end();
         });
-       
 
-        con.query(sql2, function (err, result) {
-            if (err) throw err;
-            console.log("Result(s) from query: ")
-            console.log(result[0]);
-            res.write(result[0].toString());
-            //con.end();
-        });
-        res.send();
-        con.end();
     })
 });
 
